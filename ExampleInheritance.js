d2c35c533ac1749f64b6c3222352512f9a677d9c
Hamza Firdaus <96603987+hamzafrd@users.noreply.github.com>
2022-08-04 10:59:19 +0700

    Create ExampleInheritance.js

---
+++ ExampleInheritance.js
@@ -0,0 +1,27 @@
+class Mail {
+    constructor(author) {
+        this.from = "from : " + author;
+        this._contacts = [];
+    }
+    sendMessage(msg, to) {
+        console.log(`you send: ${msg} to ${to} from ${this.from}`);
+        this._contacts.push(to);
+    }
+    showAllContacts() {
+        return this._contacts;
+    }
+}
+class WhatsApp extends Mail {
+    constructor(username, isBussinessAccount, phoneNumber) {
+        super(phoneNumber);
+        this.username = username;
+        this.isBussinessAccount =isBussinessAccount;
+    }
+    myProfile() {
+        return `my name ${this.username}, is ${this.isBussinessAccount ? 'Business' : 'Personal'} Phone Number : ${this.phoneNumber}`;
+    }
+}
+const wa1 = new WhatsApp('','', '6281111111');
+console.log(wa1.from);
+
+//output : from : 6281111111
