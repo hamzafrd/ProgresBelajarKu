d20b4238c86af4baa7eed2d8c39b1c96226a6cc4
Hamza Firdaus <96603987+hamzafrd@users.noreply.github.com>
2022-08-06 07:53:34 +0700

    Create Chaining Promise.js

---
+++ Chaining Promise.js
@@ -0,0 +1,61 @@
+function makeEspresso() {
+    checkAvailability()
+        .then((value) => {
+            console.log(value);
+            return checkStock();
+        })
+        .then((value) => {
+            console.log(value)
+            return brewCoffee();
+        })
+        .then((value) => {
+            console.log(value);
+        })
+        .catch((rejectedReason) => {
+            console.log(rejectedReason);
+        });
+}
+
+const checkAvailability = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (!state.isCoffeeMachineBusy) {
+                resolve("Mesin kopi siap digunakan.");
+            } else {
+                reject("Maaf, mesin sedang sibuk.");
+            }
+        }, 1000);
+    });
+};
+
+const state = {
+    stock: {
+        coffeeBeans: 250,
+        water: 1000,
+    },
+    isCoffeeMachineBusy: false,
+}
+
+const checkStock = () => {
+    return new Promise((resolve, reject) => {
+        state.isCoffeeMachineBusy = true;
+        setTimeout(() => {
+            if (state.stock.coffeeBeans >= 16 && state.stock.water >= 250) {
+                resolve("Stok cukup. Bisa membuat kopi.");
+            } else {
+                reject("Stok tidak cukup!");
+            }
+        }, 1500);
+    });
+};
+
+const brewCoffee = () => {
+    console.log("Membuatkan kopi Anda...")
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Kopi sudah siap!")
+        }, 2000);
+    });
+};
+
+makeEspresso();
