38fe3339fb014a005e540c0f764fa61e7fc45df1
Hamza Firdaus <96603987+hamzafrd@users.noreply.github.com>
2022-08-06 08:02:19 +0700

    Update Chaining Promise.js
    adding Promise.all(promises)

--- Chaining Promise.js
+++ Chaining Promise.js
@@ -6,6 +6,11 @@ function makeEspresso() {
         })
         .then((value) => {
             console.log(value)
+            const promises = [boilWater(), grindCoffeeBeans()];
+            return Promise.all(promises);
+        })
+        .then((value) => {
+            console.log(value)
             return brewCoffee();
         })
         .then((value) => {
@@ -57,5 +62,23 @@ const brewCoffee = () => {
         }, 2000);
     });
 };
+
+const boilWater = () => {
+    return new Promise((resolve, reject) => {
+        console.log("Memanaskan air...");
+        setTimeout(() => {
+            resolve("Air panas sudah siap!");
+        }, 2000);
+    })
+}
+
+const grindCoffeeBeans = () => {
+    return new Promise((resolve, reject) => {
+        console.log("Menggiling biji kopi...");
+        setTimeout(() => {
+            resolve("Kopi sudah siap!");
+        }, 1000);
+    })
+}
 
 makeEspresso();
